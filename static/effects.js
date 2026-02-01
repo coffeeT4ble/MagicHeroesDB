77ad3f8fe4aab19b0067f4d2fb1a06d3c6d4da86
coffeeT4ble <neilmalhotra228@gmail.com>
2026-02-02 00:02:27 +0100

    revamped the system

--- static/effects.js
+++ static/effects.js
@@ -42,23 +42,51 @@ function displayEffects(effects, container) {
       
       const img = document.createElement('img')
       img.className = 'effect-image'
-      img.src = `/static/images/effects/${effect.img}.png`
+      const src = `/static/images/effects/${effect.img}.png`
+      img.src = src
       img.alt = effect.name
+      imageCell.addEventListener("click", () => {detailedTable(effect, src)})
 
       imageCell.appendChild(img)
       imageRow.appendChild(imageCell)
     })
     table.appendChild(imageRow)
-
-    const descRow = document.createElement('tr')
-    rowEffects.forEach(effect => {
-      const descCell = document.createElement('td')
-      descCell.className = 'effect-desc-cell'
-      descCell.style.whiteSpace = 'pre-line'
-      descCell.textContent = effect.description
-      descRow.appendChild(descCell)
-    })
-    table.appendChild(descRow)
   }
   container.appendChild(table)
+}
+
+function detailedTable(effect, imageSrc) {
+  const container = document.getElementById('content')
+  container.innerHTML = ''
+  
+  const dTab = document.createElement('table')
+  dTab.setAttribute('border', '5')
+  dTab.className = 'effect-detailed-table'
+
+  const headerRow = document.createElement('tr')
+  const header = document.createElement('th')
+  header.className = 'effect-detailed-table-name'
+  header.textContent = effect.name
+  headerRow.appendChild(header)
+  dTab.appendChild(headerRow)
+
+  const imgRow = document.createElement('tr')
+  const imgCell = document.createElement('td')
+  imgCell.className = 'effect-detailed-table-image-cell'
+  const image = document.createElement('img')
+  image.className = 'effect-detailed-table-image'
+  image.src = imageSrc
+  image.alt = effect.name
+  imgCell.appendChild(image)
+  imgRow.appendChild(imgCell)
+  dTab.appendChild(imgRow)
+
+  const descRow = document.createElement('tr')
+  const desc = document.createElement('td')
+  desc.className = 'effect-detailed-table-desc'
+  desc.textContent = effect.description
+  descRow.appendChild(desc)
+  dTab.appendChild(descRow)
+
+  container.appendChild(dTab)
 }
