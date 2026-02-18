a0b573b04c66cc2d58473088972ef5e6c9d9fcc1
coffeeT4ble <neilmalhotra228@gmail.com>
2026-02-18 05:08:54 +0100

    made weapon card
    no css yet

--- static/weapons.js
+++ static/weapons.js
@@ -7,7 +7,7 @@ async function loadWeapons(container){
   try {
     const response = await fetch('/api/weapons')
     const weapons = await response.json()
-    displayWeapons(weapons, container)
+    await displayWeapons(weapons, container)
   } catch (error) {
     const errorPara = document.createElement('p')
     errorPara.textContent = 'Error loading Weapons: ' + error.message
@@ -16,18 +16,18 @@ async function loadWeapons(container){
   }
 }
 
-function displayWeapons(weapons, container) {
+async function displayWeapons(weapons, container) {
   container.innerHTML = ''
 
   const table = document.createElement('table')
   table.setAttribute('border', '3')
   table.className = 'weapons-table'
 
-  for (let i = 0; i < weapons.length; i+=3) {
+  for (let i = 0; i < weapons.length; i += 3) {
     const rowWeapons = weapons.slice(i, i + 3)
 
     const imgRow = document.createElement('tr')
-    rowWeapons.forEach(weapon => {
+    const rowPromises = rowWeapons.map(async (weapon) => {
       const imgCell = document.createElement('td')
       imgCell.setAttribute('colspan', '6')
       imgCell.className = 'weapon-image-cell'
@@ -37,51 +37,101 @@ function displayWeapons(weapons, container) {
       const src = `/static/images/weapons/${weapon.img}.png`
       img.src = src
       img.alt = weapon.name
-      const effect = loadEffectById(3)
-      console.log("test" + effect.img)
-      // event listener
 
       imgCell.appendChild(img)
       imgRow.appendChild(imgCell)
+      table.appendChild(imgRow)
+      await effectRows(weapon, table)
     })
-    table.appendChild(imgRow)
+
+    await Promise.all(rowPromises)
+    //table.appendChild(imgRow)
   }
+
   container.appendChild(table)
 }
 
-
-
-async function loadEffectById(id){
-  try{
+async function getEffect(id) {
+  try {
     const response = await fetch(`/api/effects/${id}`)
-    if (!response.ok){
-      throw new Error('Effect not found')
-    }
+    if (!response.ok) throw new Error('Effect not found')
     const effect = await response.json()
     return effect
   } catch (error) {
-    console.log("no effect found :(")
+    console.error(`Could not get img for effect ${id}:`, error.message)
     return null
   }
 }
 
-/*function displayEffects(effects, table, weapon){
-  const topNumRow = document.createElement('tr')
+async function effectRows(weapon, table) {
+  const imgArray = [
+    weapon.e_1_id,
+    weapon.e_2_id,
+    weapon.e_3_id,
+    weapon.e_4_id,
+    weapon.e_5_id,
+    weapon.e_6_id
+  ]
+  const idArray = [
+    weapon.e_1_num,
+    weapon.e_2_num,
+    weapon.e_3_num,
+    weapon.e_4_num,
+    weapon.e_5_num,
+    weapon.e_6_num
+  ]
+  const numRowOne = document.createElement('tr')
+  const rowOne = document.createElement('tr')
   for(let i = 0; i < 3; i++){
-    const topNumCell = document.createElement('td')
-    topNumCell.setAttribute('colspan', '2')
-    topNumCell.textContent = i+1
-    topNumRow.appendChild(topNumCell)
-  }
-  table.appendChild(topNumRow)
+    const numRowNum = document.createElement('td')
+    numRowNum.setAttribute('colspan', '2')
+    numRowNum.className = 'weapon-effect-row-num'
+    numRowNum.textContent = i + 1
 
-  const topEffectIds = []
-  topEffectIds.push(weapon.e_1_id, weapon.e_2_id, weapon.e_3_id)
-  const topEffectsNums = []
-  topEffectNums.push(weapon.e_1_num, weapon.e_2_num, weapon.e_3_num)
-  const topImgRow = document.createElement('tr')
-  for(i = 0; i < 3; i++){
-    const topEffectNum = document.createElement('td')
-    
+    const numCell = document.createElement('td')
+    numCell.className = 'weapon-effect-num-cell'
+    numCell.textContent = idArray[i]
+
+    const imgCell = document.createElement('td')
+    const img = document.createElement('img')
+    const imgName = await getEffect(imgArray[i])
+    const src = `/static/images/effects/${imgName.img}.png`
+    img.src = src
+    img.alt = imgName
+
+    numRowOne.appendChild(numRowNum)
+    rowOne.appendChild(numCell)
+    imgCell.appendChild(img)
+    rowOne.appendChild(imgCell)
   }
-}*/
+  table.appendChild(numRowOne)
+  table.appendChild(rowOne)
+
+  const numRowTwo = document.createElement('tr')
+  const rowTwo = document.createElement('tr')
+  for(let i = 3; i < 6; i++){
+    const numRowNum = document.createElement('td')
+    numRowNum.setAttribute('colspan', '2')
+    numRowNum.className = 'weapon-effect-row-num'
+    numRowNum.textContent = i + 1
+
+    const numCell = document.createElement('td')
+    numCell.className = 'weapon-effect-num-cell'
+    numCell.textContent = idArray[i]
+
+    const imgCell = document.createElement('td')
+    const img = document.createElement('img')
+    const imgName = await getEffect(imgArray[i])
+    const src = `/static/images/effects/${imgName.img}.png`
+    img.src = src
+    img.alt = imgName
+
+    
+    numRowTwo.appendChild(numRowNum)
+    rowTwo.appendChild(numCell)
+    imgCell.appendChild(img)
+    rowTwo.appendChild(imgCell)
+  }
+  table.appendChild(numRowTwo)
+  table.appendChild(rowTwo)
+}
